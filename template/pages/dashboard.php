bd9a57cc8cd9e4cb79ac80302ff067102f2e4fe1
Joelle Everaert <72984240+Joelle-Everaert@users.noreply.github.com>
2021-01-21 17:40:22 +0100

    clean the code

--- template/pages/dashboard.php
+++ template/pages/dashboard.php
@@ -10,31 +10,12 @@
 
     if($_SESSION['account_type'] == "ADMIN"){
         include_once('admin_profile.php');
-        echo "Bonjour " .$_SESSION['firstname']. " !<br/>";
-        $getBadges = getBadges();
-?>
-
-    <ul>
-    <?php
-        foreach($getBadges as $badge){
-            echo '<li>'.$badge['name'] . " - [" .$badge['description']. ']</li>';
-        }
-    ?>
-    </ul>
-    <ul>
-  <?php
+        
 //   SI NORMIE
     }else{
         include_once('user_profile.php'); 
-        echo "<br/>Bonjour " .$_SESSION['firstname']. " " .$_SESSION['lastname']. "!";
-        $displayYourBadge = displayYourBadge($_SESSION['user_id']);
-        
-        foreach($displayYourBadge as $value){
-            echo '<li>'.$value['name'] . " - [" .$value['description']. ']</li>';
-        }
-
               
-          }
+    }
     
  
  ?>
